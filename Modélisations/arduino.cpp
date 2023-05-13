84bf08c23642953b6394008a6ff11a2c8a614f41
Hayas92 <73119260+Hayas92@users.noreply.github.com>
2023-05-13 05:26:14 +0200

    remove slide switch

--- Modélisations/arduino.cpp
+++ Modélisations/arduino.cpp
@@ -6,33 +6,28 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(4, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ8
 int ledPin = 3; // Broche de commande de la LED
 int buttonPin = 2; // Broche du bouton poussoir
 int potentiometerPin = A0; // Broche du potentiomètre
-int switchPin = 4; // Broche de l'interrupteur à glissière
 
 void setup() {
-  pixels.begin(); 
-  pinMode(ledPin, OUTPUT); 
-  pinMode(buttonPin, INPUT_PULLUP); 
-  pinMode(potentiometerPin, INPUT); 
-  pinMode(switchPin, INPUT);
+  pixels.begin();
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(potentiometerPin, INPUT);
 }
 
 void loop() {
   // potentiomètre
   int potValue = analogRead(potentiometerPin);
-  
-  // l'interrupteur à glissière
-  int switchState = digitalRead(switchPin);
-  
+
   // état du bouton poussoir
   int buttonState = digitalRead(buttonPin);
-  
-  // Allumer ou éteindre la LED en fonction de l'état de l'interrupteur à glissière et du bouton poussoir
-  if (switchState == HIGH && buttonState == LOW) {
+
+  // Allumer ou éteindre la LED en fonction de l'état du bouton poussoir
+  if (buttonState == LOW) {
     digitalWrite(ledPin, HIGH); // Allumer la LED
   } else {
     digitalWrite(ledPin, LOW); // Éteindre la LED
   }
-  
+
   // Régler la couleur des pixels en fonction de la valeur du potentiomètre
   int red = map(potValue, 0, 1023, 0, 255);
   int green = map(potValue, 0, 1023, 0, 255);
@@ -41,6 +36,6 @@ void loop() {
     pixels.setPixelColor(i, pixels.Color(red, green, blue));
   }
   pixels.show(); // MaJ des pixels
-  
+
   delay(100); // Délai pour éviter une mise à jour trop rapide des valeurs
 }
